4ceb3204e7d06fd19dceea95bb0bcccbc4670192
nenad-radulovic <nenad.radulovic@studenti.unipd.it>
2025-05-05 15:42:36 +0200

    Fine sviluppo IntegerToRoman. Close #10

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,11 +5,27 @@
 
 package it.unipd.mtss;
 
+
+
 public class IntegerToRoman 
 {
     public static String convert(int number)
     {
-        // TODO
-        return null;
+        if(!checkInput.validaInput(number))
+            throw new IllegalArgumentException();
+
+        int[] valori =    {1000, 900, 500, 400, 100, 90,  50, 40,  10, 9,   5,  4,  1};
+        String[] simboli = {"M", "CM", "D", "CD", "C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder risultato = new StringBuilder();
+
+        for (int i = 0; i < valori.length; i++) {
+            while (number >= valori[i]) {
+                number -= valori[i];
+                risultato.append(simboli[i]);
+            }
+        }
+
+        return risultato.toString();
     }
 }
